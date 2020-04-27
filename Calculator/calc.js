54776da4df7d074d9b66543c2ec41cdb8f81160f
Kevin Paul Apostol <apostolkevin@gmail.com>
2020-04-26 20:08:08 -0700

    Added event listeners, validations and parses strings to float

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,3 +1,16 @@
 var numField1 = document.getElementById("numField1");
 var numField2 = document.getElementById("numField2");
 var ResultField = document.getElementById("resultField");
+var form = document.getElementById("xIsWhatPercentOfY");
+
+form.addEventListener("submit", function() {
+
+    if (!numField2.value || !numField2.value){
+        alert("Please enter values in the fields");
+    } else {
+        var x = parseFloat(numField1.value);
+        var y = parseFloat(numField2.value);
+        alert(x + y);
+    }
+    
+});
